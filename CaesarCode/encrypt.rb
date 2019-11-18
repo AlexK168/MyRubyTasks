fc811311af4fabe436c83a074c4ce4b451656273
AlexK168 <54994815+AlexK168@users.noreply.github.com>
2019-11-18 22:13:06 +0300

    Add files via upload
    This is a very first commit. Yep.

---
+++ CaesarCode/encrypt.rb
@@ -0,0 +1,17 @@
+map = %w{ a b c d e f g h i j k l m n o p q r s t u v w x y z }
+shift = ARGV[0].to_i
+inputFile = open "C:/Alex/Ruby/Projects/message.txt"
+outputFile = File.open( "C:/Alex/Ruby/Projects/encrypted_msg.txt", "w")
+inputFile.each do |line|
+    line.downcase.each_char do |symbol|
+        if symbol =~ /\w/
+            index = map.index(symbol)?map.index(symbol) + shift : 0
+            index = index < map.length ? index : index - map.length
+            outputFile.putc map[index]?map[index] : 0
+        else
+            outputFile.putc symbol
+        end
+    end
+end
+inputFile.close
+outputFile.close
